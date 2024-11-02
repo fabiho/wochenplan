dff08975de1fd126ca19d612fdc9737c4e06773d
Fabian Hofer <hofer-93@gmx.de>
2024-11-02 17:40:15 +0100

    Initial Commit

---
+++ Wochenplan/WochenplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WochenplanApp.swift
+//  Wochenplan
+//
+//  Created by Fabian Hofer on 02.11.24.
+//
+
+import SwiftUI
+
+@main
+struct WochenplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WochenplanTests/WochenplanTests.swift
@@ -0,0 +1,17 @@
+//
+//  WochenplanTests.swift
+//  WochenplanTests
+//
+//  Created by Fabian Hofer on 02.11.24.
+//
+
+import Testing
+@testable import Wochenplan
+
+struct WochenplanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
